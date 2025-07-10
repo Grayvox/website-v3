71b4247406d07a9d3e9ea58350e65d41a939ef45
Grayvox <144566632+Grayvox@users.noreply.github.com>
2025-07-09 20:10:55 -0400

    Add project collection basis

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,20 +2,35 @@ import { defineCollection, z } from 'astro:content';
 
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({ 
+const blog = defineCollection({
     loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
     schema: z.object({
         title: z.string(),
         tag: z.enum([
-            '#webdev', 
+            '#webdev',
             '#gamedev',
-            '#obsidian', 
-            '#writing', 
-            '#thoughts', 
+            '#obsidian',
+            '#writing',
+            '#thoughts',
             '#hardware'
         ]),
         date: z.coerce.date()
     })
- });
+});
 
-export const collections = { blog };
+const projects = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
+    schema: z.object({
+        title: z.string(),
+        builtWith: z.string(),
+        links: z.object({
+            github: z.string().optional(),
+            web: z.string().optional(),
+            docs: z.string().optional(),
+            itch: z.string().optional(),
+            steam: z.string().optional(),
+        }).optional()
+    })
+})
+
+export const collections = { blog, projects };
